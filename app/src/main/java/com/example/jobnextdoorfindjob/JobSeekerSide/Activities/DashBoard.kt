a86af00d2bb91a0349784f42a144f08ec37c5d82
shahzaibkhan1 <zaibidev471@gmail.com>
2022-09-26 17:20:40 +0500

    int

--- app/src/main/java/com/example/jobnextdoorfindjob/JobSeekerSide/Activities/DashBoard.kt
+++ app/src/main/java/com/example/jobnextdoorfindjob/JobSeekerSide/Activities/DashBoard.kt
@@ -1,16 +1,12 @@
-package com.example.jobnextdoorfindjob.JobSeekerSide.Activities;
+package com.example.jobnextdoorfindjob.JobSeekerSide.Activities
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jobnextdoorfindjob.R
 
-import android.os.Bundle;
-
-import com.example.jobnextdoorfindjob.R;
-
-public class DashBoard extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash_board);
+class DashBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
     }
 }
